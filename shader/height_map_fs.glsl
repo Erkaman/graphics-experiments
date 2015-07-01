aef256150fe8d99a371c78ff12fb88732dc0b646
Eric Arnebäck <erkastina@hotmail.com>
2015-07-01 18:44:48 +0200

    separated the phong code into a separate file.

--- shader/height_map_fs.glsl
+++ shader/height_map_fs.glsl
@@ -1,5 +1,5 @@
 
-#include "lib.glsl"
+#include "phong.glsl"
 
 out vec4 fragmentColor;
 
@@ -10,31 +10,8 @@ uniform sampler2D noiseSampler;
 in vec3 vertexColor;
 in vec2 texCoord;
 
-vec3 material_specular_color = vec3(1.0);
-float material_shininess = 25.0;
-
 in vec3 viewSpaceNormal;
 in vec3 viewSpacePosition;
-vec3 scene_ambient_light = vec3(0.6);
-vec3 scene_light = vec3(0.8);
-vec3 calculateAmbient(vec3 ambientLight, vec3 materialAmbient)
-{
-    return ambientLight *  materialAmbient;
-}
-
-vec3 calculateDiffuse(vec3 diffuseLight, vec3 materialDiffuse,
- vec3 normal, vec3 directionToLight) {
- return diffuseLight * materialDiffuse * dot(normal, directionToLight);
-}
-
-vec3 calculateSpecular(vec3 specularLight, vec3 materialSpecular,
- float materialShininess, vec3 normal,
-vec3 directionToLight, vec3 directionFromEye)
-{
- vec3 h = normalize(directionToLight - directionFromEye);
- return specularLight * materialSpecular
- * pow(max(0, dot(h, normal)), materialShininess);
-}
 
 vec3 sampleDiffuseTexture() {
     return
@@ -42,26 +19,12 @@ vec3 sampleDiffuseTexture() {
 //	vertexColor;
 }
 
+
 void main()
 {
+    vec3 shading = phongVertex(sampleDiffuseTexture(), viewSpaceNormal, viewSpaceLightPosition, viewSpacePosition);
+
     // this is our height map color.
-    vec3 ambient = sampleDiffuseTexture();
-
-
-    vec3 diffuse = sampleDiffuseTexture();
-
-    vec3 normal = normalize(viewSpaceNormal);
-    vec3 directionToLight =
-	normalize(viewSpaceLightPosition - viewSpacePosition);
-
-    vec3 specular = material_specular_color;
-
-    vec3 directionFromEye = normalize(viewSpacePosition);
-
-    vec3 shading = calculateAmbient(scene_ambient_light, ambient) +
-	calculateDiffuse(scene_light, diffuse, normal, directionToLight);
-/*	calculateSpecular(scene_light, specular, material_shininess,
-	normal, directionToLight, directionFromEye);*/
 
     fragmentColor = vec4(shading, 1.0);
 }
---
+++ shader/phong.glsl
@@ -0,0 +1,35 @@
+
+vec3 material_specular_color = vec3(1.0);
+float material_shininess = 25.0;
+
+
+vec3 scene_ambient_light = vec3(0.6);
+vec3 scene_light = vec3(0.8);
+
+vec3 calculateAmbient(vec3 ambientLight, vec3 materialAmbient)
+{
+    return ambientLight *  materialAmbient;
+}
+
+vec3 calculateDiffuse(vec3 diffuseLight, vec3 materialDiffuse,
+ vec3 normal, vec3 directionToLight) {
+ return diffuseLight * materialDiffuse * dot(normal, directionToLight);
+}
+
+vec3 phongVertex(vec3 diffuseTextureSample, vec3 viewSpaceNormal, vec3 viewSpaceLightPosition, vec3 viewSpacePosition) {
+
+    vec3 ambient = diffuseTextureSample;
+
+    vec3 diffuse = diffuseTextureSample;
+
+    vec3 normal = normalize(viewSpaceNormal);
+
+    vec3 directionToLight =
+	normalize(viewSpaceLightPosition - viewSpacePosition);
+
+    vec3 directionFromEye = normalize(viewSpacePosition);
+
+    return calculateAmbient(scene_ambient_light, ambient) +
+	calculateDiffuse(scene_light, diffuse, normal, directionToLight);
+
+}
