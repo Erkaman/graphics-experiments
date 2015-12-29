667bc4abb86ff2449100145789e251a1047c3fd5
Eric Arnebäck <erkastina@hotmail.com>
2015-12-29 15:55:24 +0100

    made simple world.

--- samples/game/src/tuhu_application.cpp
+++ samples/game/src/tuhu_application.cpp
@@ -150,7 +150,7 @@ void TuhuApplication::Init() {
 
     bool guiMode = (m_gui != 0);
 
-    if(ResourceManager::GetInstance().PathExists(dir) && false) {
+    if(ResourceManager::GetInstance().PathExists(dir)/* && false*/) {
 
 	m_heightMap = new HeightMap(
 	    File::AppendPaths(dir, HEIGHT_MAP_FILENAME ) ,
