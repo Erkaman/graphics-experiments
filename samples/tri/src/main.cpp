8cc5b87ec0bbfd2982a6d70110091c936e37aedd
Eric Arnebäck <erkastina@hotmail.com>
2016-01-30 10:48:12 +0100

    generated samples.

--- samples/tri/src/main.cpp
+++ samples/tri/src/main.cpp
@@ -1,37 +1,78 @@
 #include "ewa/log.hpp"
 
-#include "ewa/value_noise.hpp"
 #include "ewa/math/math_common.hpp"
 #include "ewa/gl/texture.hpp"
 #include "ewa/math/vector3f.hpp"
-#include "ewa/math/color.hpp"
-#include "ewa/random.hpp"
+#include "ewa/math/vector2f.hpp"
+#include "ewa/math/vector2i.hpp"
+#include "ewa/mult_array.hpp"
 
+#include "perlin.hpp"
 
-#include <vector>
-#include <string>
-#include <unordered_set>
-
-#include <stdlib.h>
 #include <math.h>
+#include <map>
+
+#include "util.hpp"
 
 using std::vector;
 using std::string;
 
-constexpr int SEED = 7;
-
-const ValueNoise noise(SEED);
-
 float SampleNoise(const Vector2f& p);
 
-void SaveNoise( const int xmin, const int xmax, const int ymin, const int ymax, const float scale, const string& filename);
+
+PerlinNoise pn;
+
+typedef MultArray<float> Samples;
+
+float NOISE_SCALE =0.02;
 
 
-inline
-float deg2rad(const float deg)
-{ return deg * M_PI / 180; }
+    float TRI_SCALE= 1.0f;;
 
-constexpr float kEpsilon = 1e-8;
+float SampleNoise(Vector2i p) {
+    return pn.noise(p.x*NOISE_SCALE,p.y*NOISE_SCALE,0);
+}
+
+float SampleNoise(uint32_t x, uint32_t y) {
+    return SampleNoise(Vector2i(x,y));
+}
+
+Vector3f v0(-1.0f * TRI_SCALE, -1.0f * TRI_SCALE, -5.0f * TRI_SCALE);
+Vector3f v1( 1.0f * TRI_SCALE, -1.0f * TRI_SCALE, -5.0f * TRI_SCALE);
+Vector3f v2( 0.0f,  1.0f * TRI_SCALE, -5.0f * TRI_SCALE);
+
+constexpr uint32_t width = 640;
+constexpr uint32_t height = 480;
+
+constexpr int GRID_X = 30;
+constexpr int GRID_Y = 30;
+
+constexpr int GRID_CELL_X_SIZE = width / GRID_X;
+constexpr int GRID_CELL_Y_SIZE = height / GRID_Y;
+
+int SAMPLES = 200;
+
+float fov = 51.52;
+
+float scale = tan(deg2rad(fov * 0.3));
+
+float imageAspectRatio = width / (float)height;
+Vector3f orig(0);
+
+bool rayTriangleIntersect(
+    uint32_t i, uint32_t j,
+
+    float &t, float &u, float &v) {
+
+    float x = (2 * (i + 0.5) / (float)width - 1) * imageAspectRatio * scale;
+    float y = (1 - 2 * (j + 0.5) / (float)height) * scale;
+    Vector3f dir(x, y, -1);
+    //cameraToWorld.multDirMatrix(Vector3f(x, y, -1), dir);
+    dir.Normalize();
+
+    return rayTriangleIntersect(orig, dir, v0, v1, v2, t, u, v);
+}
+
 
 int fact(int n) {
     if(n <= 1)
@@ -45,6 +86,16 @@ float pow(float b, int e) {
 	return 1.0;
 
     return b * pow(b,e-1);
+}
+
+float B(int n, int i, int j, int l, float s, float t) {
+
+    int nijl = fact(n) / (fact(i) * fact(j) * fact(l)  );
+    return (float)nijl *
+	pow(s, i) *
+	pow(t, j) *
+	pow(1-s-t, l);
+
 }
 
 Vector3f bezTri(int n, float s, float t, Vector3f* cols) {
@@ -62,14 +113,10 @@ Vector3f bezTri(int n, float s, float t, Vector3f* cols) {
 
 		if(i+j+l == n) {
 
-		    int nijl = fact(n) / (fact(i) * fact(j) * fact(l)  );
 
 
 		    result = result +
-			(float)nijl *
-			pow(s, i) *
-			pow(t, j) *
-			pow(1-s-t, l) * cols[a];
+			B(n, i,j,l,s,t) * cols[a];
 
 		    a++;
 
@@ -86,80 +133,15 @@ Vector3f bezTri(int n, float s, float t, Vector3f* cols) {
 
 
 
-bool rayTriangleIntersect(
-    const Vector3f &orig, const Vector3f &dir,
-    const Vector3f &v0, const Vector3f &v1, const Vector3f &v2,
-    float &t, float &u, float &v)
-{
-    // compute plane's normal
-    Vector3f v0v1 = v1 - v0;
-    Vector3f v0v2 = v2 - v0;
-    // no need to normalize
-    Vector3f N = Vector3f::Cross(v0v1,v0v2); // N
-    float denom = Vector3f::Dot(N,N);
-
-    // Step 1: finding P
-
-    // check if ray and plane are parallel ?
-    float NdotRayDirection = Vector3f::Dot(N,dir);
-    if (fabs(NdotRayDirection) < kEpsilon) // almost 0
-        return false; // they are parallel so they don't intersect !
-
-    // compute d parameter using equation 2
-    float d = Vector3f::Dot(N,v0);
-
-    // compute t (equation 3)
-    t = (  Vector3f::Dot(N,orig) + d) / NdotRayDirection;
-    // check if the triangle is in behind the ray
-    if (t < 0) return false; // the triangle is behind
-
-    // compute the intersection point using equation 1
-    Vector3f P = orig + t * dir;
-
-    // Step 2: inside-outside test
-    Vector3f C; // vector perpendicular to triangle's plane
-
-    // edge 0
-    Vector3f edge0 = v1 - v0;
-    Vector3f vp0 = P - v0;
-    C = Vector3f::Cross(edge0,vp0);
-    if (Vector3f::Dot(N, C) < 0) return false; // P is on the right side
-
-    // edge 1
-    Vector3f edge1 = v2 - v1;
-    Vector3f vp1 = P - v1;
-    C = Vector3f::Cross(edge1,vp1);
-    if ((u = Vector3f::Dot(N,C)) < 0)  return false; // P is on the right side
-
-    // edge 2
-    Vector3f edge2 = v0 - v2;
-    Vector3f vp2 = P - v2;
-    C = Vector3f::Cross(edge2,vp2);
-    if ((v = Vector3f::Dot(N,C)) < 0) return false; // P is on the right side;
-
-    u /= denom;
-    v /= denom;
-
-    return true; // this ray hits the triangle
-}
 
 int main (int, char *[]) {
 
+    srand(time(NULL));
+
+
     LogInit();
 
-
-
-
-
-
-
-
-    Vector3f v0(-1, -1, -5);
-    Vector3f v1( 1, -1, -5);
-    Vector3f v2( 0,  1, -5);
-
-    const uint32_t width = 640;
-    const uint32_t height = 480;
+    system("rm out.png");
 
 //    Vector3f cols[3] = {{0.6, 0.4, 0.1}, {0.1, 0.5, 0.3}, {0.1, 0.3, 0.7}};
     Vector3f cols1[3] = {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}};
@@ -177,34 +159,69 @@ int main (int, char *[]) {
 
     //  bezTri(2,0,0);
 
+
     unsigned char *framebuffer = new unsigned char[width * height * 4];
     unsigned char *pix = framebuffer;
-    float fov = 51.52;
-    float scale = tan(deg2rad(fov * 0.5));
-    float imageAspectRatio = width / (float)height;
-    Vector3f orig(0);
+
+    Samples samples(width, height,0.0f);
+    MultArray<bool> samplesGrid(GRID_X, GRID_Y, false);
+    int acceptedSamples = 0;
+
+
+    while(acceptedSamples < SAMPLES) {
+
+	uint32_t i = rand() % width;
+	uint32_t j = rand() % height;
+
+            float t, u, v;
+
+	if (!rayTriangleIntersect(i,j,t, u, v))
+	    continue; // reject
+
+//	const Vector2i point(i,j);
+
+
+//	LOG_I("grid: %d, %d", i,j);
+
+
+	int gridX = i / GRID_CELL_X_SIZE;
+	int gridY = j / GRID_CELL_Y_SIZE;
+
+//	LOG_I("grid: %d, %d", gridX, gridY);
+
+	if(samplesGrid(gridX, gridY) == true  )
+	    continue; // reject.
+
+	samplesGrid(gridX, gridY) = true;
+
+	samples(i,j) = SampleNoise(i,j);
+	++acceptedSamples;
+
+    }
+
     for (uint32_t j = 0; j < height; ++j) {
         for (uint32_t i = 0; i < width; ++i) {
             // compute primary ray
-            float x = (2 * (i + 0.5) / (float)width - 1) * imageAspectRatio * scale;
-            float y = (1 - 2 * (j + 0.5) / (float)height) * scale;
-            Vector3f dir(x, y, -1);
-            //cameraToWorld.multDirMatrix(Vector3f(x, y, -1), dir);
-            dir.Normalize();
+
             float t, u, v;
 
-
 	    Vector3f col;
+	    unsigned char alpha;
 
-            if (rayTriangleIntersect(orig, dir, v0, v1, v2, t, u, v)) {
+            if (rayTriangleIntersect(i,j,t, u, v)) {
 
 //                col = u * cols[0] + v * cols[1] + (1 - u - v) * cols[2];
 
 		float s = u;
 		float t = v;
 
-                col =
-		    bezTri(1, s, t, cols1);
+
+                col = bezTri(1, s, t, cols1);
+
+
+		float sample = pn.noise(i*NOISE_SCALE,j*NOISE_SCALE,0);
+		col =Vector3f(sample);
+
 
 //		    s * cols[0] + t * cols[1] + (1 - s - t) * cols[2];
 
@@ -217,14 +234,23 @@ int main (int, char *[]) {
 
 
                 //*pix = Vector3f(u, v, 1 - u - v);
+
+		alpha = 255;
+
+		if(samples(i,j) > 0.001) {
+		    col = Vector3f(1,1,0);
+		}
+
+
             } else {
 		col = Vector3f(0,0,0);
+		alpha = 0;
 	    }
 
 	    *(pix++) = (unsigned char)((col.x) * 255.0f);
 	    *(pix++) = (unsigned char)((col.y) * 255.0f);
 	    *(pix++) = (unsigned char)((col.z) * 255.0f);
-	    *(pix++) = (unsigned char)(255);
+	    *(pix++) = (unsigned char)(alpha);
         }
     }
 
@@ -241,57 +267,3 @@ int main (int, char *[]) {
 
     return 0;
 }
-/*
-float SampleNoise(const Vector2f& p) {
-
-    float sample = noise.Turbulence(7, p * 0.04f, 2.0f, 0.5f);
-
-    sample -= 0.4f;
-
-    if(sample < 0.0f) {
-	sample = 0.0f;
-    }
-
-    sample = 1.0f - ( pow(0.1f,sample)  * 1.0f );
-
-    return sample;
-    }*/
-
-/*
-void SaveNoise( const int xmin, const int xmax, const int ymin, const int ymax, const float scale, const string& filename) {
-
-    const size_t height = (size_t)((ymax - ymin) * scale + 1);
-    const size_t width = (size_t)((xmax - xmin) * scale + 1);
-
-    unsigned char* pixels = new unsigned char[4 * height * width];
-
-    int i = 0;
-
-    for(size_t y = 0; y < height; y += 1) {
-	for(size_t x = 0; x < width; x += 1) {
-
-	    const Vector2f p(
-		(x)/scale + xmin,
-		(y)/scale + ymin);
-	    const float sample =  SampleNoise(p);
-
-	    Color sky(1.0f, 1.0f, 1.0f);
-	    Color clouds(sample,sample,sample);
-
-	//    unsigned char alpha = sample < EPSILON ? 0 : CLOUD_ALPHA;
-//	    unsigned char val = (unsigned char)((sample) * 255.0f);
-
-	    Color l = Color::Lerp(sky, clouds, 0.3f);
-
-	    pixels[i++] = (unsigned char)((l.r) * 255.0f);
-	    pixels[i++] = (unsigned char)((l.g) * 255.0f);
-	    pixels[i++] =(unsigned char)((l.b) * 255.0f);
-	    pixels[i++] = (unsigned char)(( Clamp(sample-0.1f, 0.0f,1.0f) ) * 255.0f);
-	}
-    }
-
-    Texture::WriteToFile(pixels,width,height,filename);
-
-    delete pixels;
-}
-*/
---
+++ samples/tri/src/util.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+constexpr float kEpsilon = 1e-8;
+
+inline
+float deg2rad(const float deg)
+{ return deg * M_PI / 180; }
+
+
+bool rayTriangleIntersect(
+    const Vector3f &orig, const Vector3f &dir,
+    const Vector3f &v0, const Vector3f &v1, const Vector3f &v2,
+    float &t, float &u, float &v)
+{
+    // compute plane's normal
+    Vector3f v0v1 = v1 - v0;
+    Vector3f v0v2 = v2 - v0;
+    // no need to normalize
+    Vector3f N = Vector3f::Cross(v0v1,v0v2); // N
+    float denom = Vector3f::Dot(N,N);
+
+    // Step 1: finding P
+
+    // check if ray and plane are parallel ?
+    float NdotRayDirection = Vector3f::Dot(N,dir);
+    if (fabs(NdotRayDirection) < kEpsilon) // almost 0
+        return false; // they are parallel so they don't intersect !
+
+    // compute d parameter using equation 2
+    float d = Vector3f::Dot(N,v0);
+
+    // compute t (equation 3)
+    t = (  Vector3f::Dot(N,orig) + d) / NdotRayDirection;
+    // check if the triangle is in behind the ray
+    if (t < 0) return false; // the triangle is behind
+
+    // compute the intersection point using equation 1
+    Vector3f P = orig + t * dir;
+
+    // Step 2: inside-outside test
+    Vector3f C; // vector perpendicular to triangle's plane
+
+    // edge 0
+    Vector3f edge0 = v1 - v0;
+    Vector3f vp0 = P - v0;
+    C = Vector3f::Cross(edge0,vp0);
+    if (Vector3f::Dot(N, C) < 0) return false; // P is on the right side
+
+    // edge 1
+    Vector3f edge1 = v2 - v1;
+    Vector3f vp1 = P - v1;
+    C = Vector3f::Cross(edge1,vp1);
+    if ((u = Vector3f::Dot(N,C)) < 0)  return false; // P is on the right side
+
+    // edge 2
+    Vector3f edge2 = v0 - v2;
+    Vector3f vp2 = P - v2;
+    C = Vector3f::Cross(edge2,vp2);
+    if ((v = Vector3f::Dot(N,C)) < 0) return false; // P is on the right side;
+
+    u /= denom;
+    v /= denom;
+
+    return true; // this ray hits the triangle
+}
