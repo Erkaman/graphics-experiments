192a9338992bcb46b1281ff50e9cdacfeebcf950
Eric Arnebäck <erkastina@hotmail.com>
2016-02-03 20:35:36 +0100

    refactor gui.

--- samples/game/src/imgui_wrapper.hpp
+++ samples/game/src/imgui_wrapper.hpp
@@ -2,5 +2,19 @@
 
 #include <imgui.h>
 
-#include <GL/glew.h> // include GLEW and new version of GL on Windows
-#include <GLFW/glfw3.h> // GLFW helper library
+class GLFWwindow;
+
+bool    ImGui_ImplGlfwGL3_Init(GLFWwindow* window, bool install_callbacks);
+
+bool ImGui_ImplGlfwGL3_CreateDeviceObjects();
+
+
+void ImGui_ImplGlfwGL3_InvalidateDeviceObjects();
+
+void ImGui_ImplGlfwGL3_NewFrame(float guiVerticalScale);
+
+GLFWwindow* ImGui_ImplGlfwGL3_GetWindow();
+
+float GetDeltaScroll();
+
+void SetDeltaScroll(float deltaScroll_);
